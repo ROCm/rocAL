95e06532e96c82ee913356baa2af069584364e99
kiritigowda <kiritigowda@gmail.com>
2022-12-30 11:21:05 -0800

    PyBind Install - Dir Independent

--- rocAL_pybind/run.sh
+++ rocAL_pybind/run.sh
@@ -22,6 +22,8 @@
 
 PYTHON_VERSION=`python3 -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";` ##Gets the Python3 version
 DEFAULT_PYTHON=$(which python$PYTHON_VERSION) ## Gets the default python
+SCRIPT=$(readlink -f "$0") ## path to run.sh
+SCRIPTPATH=$(dirname "$SCRIPT") ## script location
 
 sudo rm -r ./amd_rocal.egg-info/
 sudo rm -r ./build
@@ -31,11 +33,11 @@ blue=`tput setaf 4`
 reset=`tput sgr0`
 
 if [[ $# -eq 1 ]]; then
-  # Either --backend_hip / --backend_ocl can be passed by the user
-  echo "${blue}Running setup.py with $1 ${reset}"
+  # Backend --backend_hip / --backend_ocl / --backend_cpu can be passed by the user
+  echo "${blue}Running "$SCRIPTPATH"/setup.py with $1 ${reset}"
 
-  if [[ "$1" == "--backend_ocl" ]] || [[ "$1" == "--backend_hip" ]]; then
-    sudo "$DEFAULT_PYTHON" setup.py bdist_wheel $1
+  if [[ "$1" == "--backend_ocl" ]] || [[ "$1" == "--backend_hip" ]] || [[ "$1" == "--backend_cpu" ]]; then
+    sudo "$DEFAULT_PYTHON" "$SCRIPTPATH"/setup.py bdist_wheel $1
     WHEEL_DIR="./dist"
     WHEEL_NAME=""
     for WHEEL_NAME in "$WHEEL_DIR"/*
@@ -46,17 +48,17 @@ if [[ $# -eq 1 ]]; then
     python$PYTHON_VERSION -m pip install $WHEEL_NAME
   else
     echo
-    echo "The run.sh bash script runs the setup.py with OCL / HIP backends"
+    echo "The run.sh bash script runs the setup.py with OCL / HIP / CPU backends"
     echo
-    echo "Syntax : ./run.sh --backend_ocl / --backend_hip"
+    echo "Syntax : ./run.sh --backend_ocl / --backend_hip / --backend_cpu"
     echo
     exit
   fi
 
 else
   # Default Backend: --backend_hip
-  echo "${blue}Running setup.py with --backend_hip ${reset}"
-  sudo "$DEFAULT_PYTHON" setup.py bdist_wheel --backend_hip
+  echo "${blue}Running "$SCRIPTPATH"/setup.py with --backend_hip ${reset}"
+  sudo "$DEFAULT_PYTHON" "$SCRIPTPATH"/setup.py bdist_wheel --backend_hip
   WHEEL_DIR="./dist"
   WHEEL_NAME=""
   for WHEEL_NAME in "$WHEEL_DIR"/*
---
+++ third_party/README.md
@@ -0,0 +1,5 @@
+## rocAL Third-Party Dependency
+
+### RapidJSON
+
+RapidJSON is a JSON parser and generator for C++.
