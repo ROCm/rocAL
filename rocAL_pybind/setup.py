9feb3f59b78ab3518ca1937916747b7f85044a34
LakshmiKumar23 <lakshmi.kumar@amd.com>
2023-01-12 16:04:14 -0800

    initial commit for rocal_pybind cmake changes

--- rocAL_pybind/setup.py
+++ rocAL_pybind/setup.py
@@ -18,72 +18,21 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from __future__ import absolute_import
-# from setuptools import setup
-import os
+from setuptools import setup, find_packages, Extension
+from setuptools.dist import Distribution
 import sys
-import subprocess
-
-from setuptools import setup, Extension, find_packages
-from setuptools.command.build_ext import build_ext
-
-scriptPath = os.path.dirname(os.path.realpath(__file__))
-
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=scriptPath):
-        Extension.__init__(self, name, sources=[])
-        if "--backend_hip" in sys.argv:
-            self.backend = "HIP"
-            sys.argv.remove("--backend_hip")
-        elif "--backend_ocl" in sys.argv:
-            self.backend = "OCL"
-            sys.argv.remove("--backend_ocl")
-        elif "--backend_cpu" in sys.argv:
-            self.backend = "CPU"
-            sys.argv.remove("--backend_cpu")
-
-class CMakeBuild(build_ext):
-    def run(self):
-        try:
-            subprocess.check_output(['cmake', '--version'])
-        except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
-
-        for ext in self.extensions:
-            self.build_extension(ext)
-
-    def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(
-            self.get_ext_fullpath(ext.name)))
-        if ext.backend == "HIP":
-            cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                          '-DPYTHON_EXECUTABLE=' + sys.executable, '-DGPU_SUPPORT=ON', '-DBACKEND=HIP']
-        elif ext.backend == "OCL":
-            cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                          '-DPYTHON_EXECUTABLE=' + sys.executable, '-DGPU_SUPPORT=ON', '-DBACKEND=OPENCL']
-        elif ext.backend == "CPU":
-            cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                          '-DPYTHON_EXECUTABLE=' + sys.executable, '-DGPU_SUPPORT=OFF', '-DBACKEND=CPU']
-
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
-
-        cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-        build_args += ['--', '-j2']
-
-        env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', scriptPath] +
-                              cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] +
-                              build_args, cwd=self.build_temp)
 
 
-setup(name='amd-rocal',
+if sys.version_info < (3, 0):
+    sys.exit('rocal Python Package requires Python > 3.0')
+
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+    def has_ext_modules(foo):
+        return True
+
+setup(
+      name='amd-rocal',
       description='AMD ROCm Augmentation Library',
       url='https://github.com/ROCmSoftwarePlatform/rocAL',
       version='1.0.0',
@@ -91,7 +40,7 @@ setup(name='amd-rocal',
       license='Apache License 2.0',
       packages=find_packages(),
       include_package_data=True,
-      ext_modules=[CMakeExtension('rocal_pybind')],
-      cmdclass=dict(build_ext=CMakeBuild),
+      ext_modules=[Extension('rocal_pybind')],
       zip_safe=False,
+      distclass=BinaryDistribution
       )
