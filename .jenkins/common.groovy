4024eb7c660bd8592c9e21dfb45b7345c0908c22
Kiriti Gowda <kiritigowda@gmail.com>
2023-07-21 16:39:51 -0700

    5.7 - Udates Sync (#40)
    * 5.7 - Udates Sync
    
    * Jenkins

---
+++ .jenkins/common.groovy
@@ -0,0 +1,83 @@
+// This file is for internal AMD use.
+// If you are interested in running your own Jenkins, please raise a github issue for assistance.
+
+def runCompileCommand(platform, project, jobName, boolean debug=false, boolean staticLibrary=false) {
+    project.paths.construct_build_prefix()
+
+    String buildTypeArg = debug ? '-DCMAKE_BUILD_TYPE=Debug' : '-DCMAKE_BUILD_TYPE=Release'
+    String buildTypeDir = debug ? 'debug' : 'release'
+    String backend = 'HIP'
+    String enableSCL = 'echo build-rocAL'
+
+    if (platform.jenkinsLabel.contains('centos')) {
+        backend = 'CPU'
+        if (platform.jenkinsLabel.contains('centos7')) {
+            enableSCL = 'source scl_source enable llvm-toolset-7'
+        }
+    }
+    else if (platform.jenkinsLabel.contains('ubuntu20')) {
+        backend = 'OCL'
+    }
+
+    def command = """#!/usr/bin/env bash
+                set -x
+                echo Build rocAL - ${buildTypeDir}
+                cd ${project.paths.project_build_prefix}
+                python rocAL-setup.py
+                mkdir -p build/${buildTypeDir} && cd build/${buildTypeDir}
+                ${enableSCL}
+                cmake -DBACKEND=${backend} ${buildTypeArg} ../..
+                make -j\$(nproc)
+                sudo make install
+                sudo make package
+                """
+
+    platform.runCommand(this, command)
+}
+
+def runTestCommand (platform, project) {
+
+    def command = """#!/usr/bin/env bash
+                set -x
+                ldd -v /opt/rocm/lib/librocal.so
+                """
+
+    platform.runCommand(this, command)
+// Unit tests - TBD
+}
+
+def runPackageCommand(platform, project) {
+
+    def packageHelper = platform.makePackage(platform.jenkinsLabel, "${project.paths.project_build_prefix}/build/release")
+
+    String packageType = ""
+    String packageInfo = ""
+
+    if (platform.jenkinsLabel.contains('centos') ||
+        platform.jenkinsLabel.contains('rhel') ||
+        platform.jenkinsLabel.contains('sles')) {
+        packageType = 'rpm'
+        packageInfo = 'rpm -qlp'
+        }
+    else
+    {
+        packageType = 'deb'
+        packageInfo = 'dpkg -c'
+    }
+
+    def command = """#!/usr/bin/env bash
+                set -x
+                export HOME=/home/jenkins
+                echo Make rocAL Package
+                cd ${project.paths.project_build_prefix}/build/release
+                sudo make package
+                mkdir -p package
+                mv *.${packageType} package/
+                ${packageInfo} package/*.${packageType}
+                """
+
+    platform.runCommand(this, command)
+    platform.archiveArtifacts(this, packageHelper[1])
+}
+
+return this
