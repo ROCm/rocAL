2c4469a4a8c86d345309700c059ebf9a2b85c923
Swetha B S <swetha@multicorewareinc.com>
2023-11-15 18:29:42 +0000

    Put all the examples in a single file

--- rocAL_pybind/examples/rocAL_external_source/rocAL_api_external_source_mode0_example.py
+++ rocAL_pybind/examples/rocAL_external_source/rocAL_api_external_source_mode0_example.py
@@ -6,6 +6,8 @@ import amd.rocal.types as types
 import os
 import numpy as np
 import cupy as cp
+import cv2
+
 
 
 def main():
@@ -22,7 +24,6 @@ def main():
         print(error)
 
     def image_dump(img, idx, device="cpu"):
-        import cv2
         if device == "gpu":
             img = cp.asnumpy(img)
         img = img.transpose([1, 2, 0])  # NCHW
@@ -31,6 +32,7 @@ def main():
         cv2.imwrite("OUTPUT_IMAGES_PYTHON/EXTERNAL_SOURCE_READER/MODE0/" +
                     str(idx)+"_"+"train"+".png", img)
 
+    ##################### MODE 0 #########################
     # Define the Data Source for all image samples - User needs to define their own source
     class ExternalInputIteratorMode0(object):
         def __init__(self, batch_size):
@@ -64,7 +66,7 @@ def main():
     external_input_source = ExternalInputIteratorMode0(batch_size)
 
     # Create the pipeline
-    external_source_pipeline_mode0 = Pipeline(batch_size=batch_size, num_threads=1, device_id=0,
+    external_source_pipeline_mode0 = Pipeline(batch_size=batch_size, num_threads=1, device_id=0, prefetch_queue_depth=4,
                                               seed=1, rocal_cpu=True if device == "cpu" else False, tensor_layout=types.NCHW)
 
     with external_source_pipeline_mode0:
@@ -82,6 +84,7 @@ def main():
     data_loader = ROCALClassificationIterator(
         external_source_pipeline_mode0, device=device)
     for i, output_list in enumerate(data_loader, 0):
+        print("**************MODE 0*******************")
         print("**************", i, "*******************")
         print("**************starts*******************")
         print("\nImages:\n", output_list)
@@ -91,6 +94,169 @@ def main():
             cnt = cnt + 1
             image_dump(img, cnt, device=device)
 
+    ##################### MODE 0 #########################
+    
+    ##################### MODE 1 #########################
+        # Define the Data Source for all image samples
+    class ExternalInputIteratorMode1(object):
+        def __init__(self, batch_size):
+            self.images_dir = data_dir
+            self.batch_size = batch_size
+            self.files = []
+            import os
+            import glob
+            for filename in glob.glob(os.path.join(self.images_dir, '*.jpg')):
+                self.files.append(filename)
 
+        def __iter__(self):
+            self.i = 0
+            self.n = len(self.files)
+            self.maxWidth = None
+            self.maxHeight = None
+            return self
+
+        def __next__(self):
+            batch = []
+            labels = []
+            srcsize_height = []
+            label = 1
+            for x in range(self.batch_size):
+                jpeg_filename = self.files[self.i]
+                f = open(jpeg_filename, 'rb')
+                numpy_buffer = np.frombuffer(f.read(), dtype=np.uint8)
+                batch.append(numpy_buffer)
+                srcsize_height.append(len(numpy_buffer))
+                labels.append(label)
+                label = label + 1
+                self.i = (self.i + 1) % self.n
+            labels = np.array(labels).astype('int32')
+            return (batch, labels, srcsize_height)
+
+# Mode 1
+    eii_1 = ExternalInputIteratorMode1(batch_size)
+
+    # Create the pipeline
+    external_source_pipeline_mode1 = Pipeline(batch_size=batch_size, num_threads=1, device_id=0, prefetch_queue_depth=4,
+                                              seed=1, rocal_cpu=True if device == "cpu" else False, tensor_layout=types.NCHW)
+
+    with external_source_pipeline_mode1:
+        jpegs, _ = fn.external_source(
+            source=eii_1, mode=types.EXTSOURCE_RAW_COMPRESSED, max_width=2000, max_height=2000)
+        output = fn.resize(jpegs, resize_width=2000, resize_height=2000,
+                           output_layout=types.NCHW, output_dtype=types.UINT8)
+        external_source_pipeline_mode1.set_outputs(output)
+
+    # build the external_source_pipeline_mode1
+    external_source_pipeline_mode1.build()
+    # Index starting from 0
+    cnt = 0
+    # Dataloader
+    data_loader = ROCALClassificationIterator(
+        external_source_pipeline_mode1, device=device)
+    for i, output_list in enumerate(data_loader, 0):
+        print("**************MODE 1*******************")
+        print("**************", i, "*******************")
+        print("**************starts*******************")
+        print("\nImages:\n", output_list)
+        print("**************ends*******************")
+        print("**************", i, "*******************")
+        for img in output_list[0][0]:
+            cnt = cnt + 1
+            image_dump(img, cnt, device=device)
+    ##################### MODE 1 #########################
+    
+    ##################### MODE 2 #########################
+        # Define the Data Source for all image samples
+
+    class ExternalInputIteratorMode2(object):
+        def __init__(self, batch_size):
+            self.images_dir = data_dir
+            self.batch_size = batch_size
+            self.files = []
+            self.maxHeight = self.maxWidth = 0
+            import os
+            import glob
+            for filename in glob.glob(os.path.join(self.images_dir, '*.jpg')):
+                self.files.append(filename)
+            shuffle(self.files)
+            self.i = 0
+            self.n = len(self.files)
+            for x in range(self.n):
+                jpeg_filename = self.files[x]
+                label = 1
+                image = cv2.imread(jpeg_filename, cv2.IMREAD_COLOR)
+                # Check if the image was loaded successfully
+                if image is None:
+                    print("Error: Failed to load the image.")
+                else:
+                    # Get the height and width of the image
+                    height, width = image.shape[:2]
+                self.maxHeight = height if height > self.maxHeight else self.maxHeight
+                self.maxWidth = width if width > self.maxWidth else self.maxWidth
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            batch = []
+            batch_of_numpy = []
+            labels = []
+            label = 1
+            roi_height = []
+            roi_width = []
+            self.out_image = np.zeros(
+                (self.batch_size, self.maxHeight, self.maxWidth, 3), dtype="uint8")
+            for x in range(self.batch_size):
+                jpeg_filename = self.files[self.i]
+                image = cv2.imread(jpeg_filename, cv2.IMREAD_COLOR)
+                # Check if the image was loaded successfully
+                if image is None:
+                    print("Error: Failed to load the image.")
+                else:
+                    # Get the height and width of the image
+                    height, width = image.shape[:2]
+                batch.append(np.asarray(image))
+                roi_height.append(height)
+                roi_width.append(width)
+                self.out_image[x][:roi_height[x], :roi_width[x], :] = batch[x]
+                batch_of_numpy.append(self.out_image[x])
+                labels.append(label)
+                label = label + 1
+                self.i = (self.i + 1) % self.n
+            labels = np.array(labels).astype('int32')
+            return (batch_of_numpy, labels, roi_height, roi_width, self.maxHeight, self.maxWidth)
+
+
+# Mode 2
+    eii_2 = ExternalInputIteratorMode2(batch_size)
+
+    # Create the pipeline
+    external_source_pipeline_mode2 = Pipeline(batch_size=batch_size, num_threads=1, device_id=0, prefetch_queue_depth=4,
+                                              seed=1, rocal_cpu=True if device == "cpu" else False, tensor_layout=types.NCHW)
+
+    with external_source_pipeline_mode2:
+        jpegs, _ = fn.external_source(source=eii_2, mode=types.EXTSOURCE_RAW_UNCOMPRESSED,
+                                      max_width=eii_2.maxWidth, max_height=eii_2.maxHeight)
+        output = fn.resize(jpegs, resize_width=300, resize_height=300,
+                           output_layout=types.NCHW, output_dtype=types.UINT8)
+        external_source_pipeline_mode2.set_outputs(output)
+
+    # build the external_source_pipeline_mode2
+    external_source_pipeline_mode2.build()
+    # Index starting from 0
+    cnt = 0
+    # Dataloader
+    data_loader = ROCALClassificationIterator(
+        external_source_pipeline_mode2, device=device)
+    for i, output_list in enumerate(data_loader, 0):
+        print("**************", i, "*******************")
+        print("**************starts*******************")
+        print("\nImages:\n", output_list)
+        print("**************ends*******************")
+        print("**************", i, "*******************")
+        for img in output_list[0][0]:
+            cnt = cnt+1
+            image_dump(img, cnt, device=device)
+    ##################### MODE 2 #########################
 if __name__ == '__main__':
     main()
