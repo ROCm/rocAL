bcb050fc7f525bc2c5911371526b3ac246e4fdcf
SundarRajan98 <svaithiy@amd.com>
2023-11-27 13:42:20 +0000

    Resizing file headers after last batch padding

--- rocAL/source/readers/image/numpy_data_reader.cpp
+++ rocAL/source/readers/image/numpy_data_reader.cpp
@@ -71,6 +71,7 @@ Reader::Status NumpyDataReader::initialize(ReaderConfig desc) {
             replicate_last_batch_to_pad_partial_shard();
         }
     }
+    _file_headers.resize(_file_names.size());
     // shuffle dataset if set
     _shuffle_time.start();
     if (ret == Reader::Status::OK && _shuffle)
@@ -434,7 +435,6 @@ Reader::Status NumpyDataReader::subfolder_reading() {
         replicate_last_image_to_fill_last_shard();
         LOG("NumpyDataReader ShardID [" + TOSTR(_shard_id) + "] Replicated " + _folder_path + _last_file_name + " " + TOSTR((_batch_count - _in_batch_read_count)) + " times to fill the last batch")
     }
-    _file_headers.resize(_file_names.size());
     if (!_file_names.empty())
         LOG("NumpyDataReader ShardID [" + TOSTR(_shard_id) + "] Total of " + TOSTR(_file_names.size()) + " images loaded from " + _full_path)
     return ret;
