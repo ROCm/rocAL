5f627b25d4c8ca4c5dc40f7d67df08376c729f2a
Swetha B S <swetha@multicorwareinc.com>
2024-03-12 07:14:51 +0000

    Change the file_path() function to virtual from pure virtual

--- rocAL/include/readers/video/sequence_file_source_reader.h
+++ rocAL/include/readers/video/sequence_file_source_reader.h
@@ -56,9 +56,6 @@ class SequenceFileSourceReader : public Reader {
     //! Returns the name of the latest file opened
     std::string id() override { return _last_id; }
 
-    //! Returns the name of the latest file_path opened
-    std::string file_path() override { return _last_file_name; }
-
     unsigned count_items() override;
 
     ~SequenceFileSourceReader() override;
@@ -83,7 +80,7 @@ class SequenceFileSourceReader : public Reader {
     unsigned _curr_file_idx;
     FILE *_current_fPtr;
     unsigned _current_file_size;
-    std::string _last_id, _last_file_name;
+    std::string _last_id;
     std::vector<std::string> _last_sequence;
     size_t _sequence_length;
     size_t _step;
