4d6982a45832d882f9e63ff38ca30a1958775daa
fiona-gladwin <fionagladwin@multicorewareinc.com>
2024-03-20 06:32:39 -0400

    Remove max frames and channels from decoder

--- rocAL_pybind/amd/rocal/decoders.py
+++ rocAL_pybind/amd/rocal/decoders.py
@@ -403,7 +403,7 @@ def image_slice(*inputs, file_root='', path='', annotations_file='', shard_id=0,
             Pipeline._current_pipeline._handle, *(kwargs_pybind.values()))
     return (image_decoder_slice)
 
-def audio(*inputs, file_root='', file_list_path='', bytes_per_sample_hint=[0], shard_id=0, num_shards=1, random_shuffle=False, downmix=False, dtype=types.FLOAT, quality=50.0, max_frames=1 , max_channels=1 ,sample_rate=0.0, seed=1, stick_to_shard=False, shard_size=-1):
+def audio(*inputs, file_root='', file_list_path='', bytes_per_sample_hint=[0], shard_id=0, num_shards=1, random_shuffle=False, downmix=False, dtype=types.FLOAT, quality=50.0, sample_rate=0.0, seed=1, stick_to_shard=False, shard_size=-1):
     kwargs_pybind = {
             "source_path": file_root,
             "shard_id": shard_id,
@@ -411,9 +411,6 @@ def audio(*inputs, file_root='', file_list_path='', bytes_per_sample_hint=[0], s
             "is_output": False,
             "shuffle": random_shuffle,
             "loop": False,
-            "downmix": downmix,
-            "max_frames": max_frames,
-            "max_channels": max_channels,
-            }
+            "downmix": downmix}
     decoded_audio = b.audioDecoderSingleShard(Pipeline._current_pipeline._handle, *(kwargs_pybind.values()))
     return decoded_audio
