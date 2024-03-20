253a5086d0ed3e6c40788b8a3c086793e6070ac1
Swetha B S <swetha@multicorwareinc.com>
2024-03-20 03:36:42 -0400

    Clean up code in audio python unit test

--- rocAL_pybind/examples/rocAL_api_audio_unittest.py
+++ rocAL_pybind/examples/rocAL_api_audio_unittest.py
@@ -6,20 +6,15 @@ import random
 import numpy as np
 from amd.rocal.plugin.pytorch import ROCALAudioIterator
 import torch
-# torch.set_printoptions(threshold=10_000)
 np.set_printoptions(threshold=1000, edgeitems=10000)
 from amd.rocal.pipeline import Pipeline
 import amd.rocal.fn as fn
-import amd.rocal.types as types
-import math
-# import rocal_pybind.tensor
 import sys
-import cv2
 import matplotlib.pyplot as plt
 import os
-def draw_patches(img, idx, device):
+
+def draw_patches(img, idx):
     #image is expected as a tensor, bboxes as numpy
-    import cv2
     image = img.detach().numpy()
     audio_data = image.flatten()
     label = idx
@@ -32,28 +27,28 @@ def draw_patches(img, idx, device):
     plt.plot(audio_data)
     plt.savefig("results/rocal_data_new"+str(label)+".png")
     plt.close()
+
 def main():
     if  len(sys.argv) < 3:
-        print ('Please pass audio_folder file_list cpu/gpu batch_size')
+        print ('Please pass audio_folder batch_size')
         exit(0)
     try:
-        path= "OUTPUT_IMAGES_PYTHON/NEW_API/FILE_READER/" + "audio"
+        path= "results"
         isExist = os.path.exists(path)
         if not isExist:
             os.makedirs(path)
     except OSError as error:
         print(error)
     data_path = sys.argv[1]
-    file_list = " "
     _rocal_cpu = True # The GPU support for Audio is not given yet
-    batch_size = int(sys.argv[4])
+    batch_size = int(sys.argv[2])
     num_threads = 1
     device_id = 0
     random_seed = random.SystemRandom().randint(0, 2**32 - 1)
     print("*********************************************************************")
     audio_pipeline = Pipeline(batch_size=batch_size, num_threads=num_threads, device_id=device_id, seed=random_seed, rocal_cpu=_rocal_cpu)
     with audio_pipeline:
-        audio_decode = fn.decoders.audio(file_root=data_path, file_list_path=file_list, downmix=False, shard_id=0, num_shards=2, stick_to_shard=False)
+        audio_decode = fn.decoders.audio(file_root=data_path, downmix=False, shard_id=0, num_shards=2, stick_to_shard=False)
         audio_pipeline.set_outputs(audio_decode)
     audio_pipeline.build()
     audioIteratorPipeline = ROCALAudioIterator(audio_pipeline, auto_reset=True)
@@ -68,7 +63,7 @@ def main():
                     print("label", label)
                     print("cnt", cnt)
                     print("img", img)
-                    draw_patches(img, cnt, "cpu")
+                    draw_patches(img, cnt)
                     cnt+=1
         print("EPOCH DONE", e)
 if __name__ == '__main__':
