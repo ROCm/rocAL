edf4b77219052816918717d65d3ff4a34c602e8d
kiritigowda <kiritigowda@gmail.com>
2024-04-11 10:20:05 -0700

    Fix - Setup.pu

--- rocAL_pybind/setup.py
+++ rocAL_pybind/setup.py
@@ -48,6 +48,6 @@ setup(
     packages=find_packages(where='@TARGET_NAME@'),
     package_dir={'amd': '@TARGET_NAME@/amd'},
     include_package_data=True,
-    ext_modules=[Extension('rocal_pybind',sources=['rocal_pybind.cpp'], include_dirs=['@pybind11_INCLUDE_DIRS@', ROCM_PATH+'/include', '@PROJECT_SOURCE_DIR@/../rocAL/include/api']
+    ext_modules=[Extension('rocal_pybind',sources=['rocal_pybind.cpp'], include_dirs=['@pybind11_INCLUDE_DIRS@', ROCM_PATH+'/include', '@PROJECT_SOURCE_DIR@/../rocAL/include/api'])],
     distclass=BinaryDistribution
 )
