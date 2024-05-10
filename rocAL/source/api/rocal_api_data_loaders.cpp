0822f76d93e8136c9ffeb95a2564afc6d85dc082
fiona-gladwin <fionagladwin@multicorewareinc.com>
2024-05-10 03:52:42 -0400

    Fix build issue

--- rocAL/source/api/rocal_api_data_loaders.cpp
+++ rocAL/source/api/rocal_api_data_loaders.cpp
@@ -2166,7 +2166,7 @@ rocalAudioFileSource(
     RocalContext p_context,
     const char* source_path,
     const char* source_file_list_path,
-    unsigned internal_shard_count,
+    unsigned shard_count,
     bool is_output,
     bool shuffle,
     bool loop,
