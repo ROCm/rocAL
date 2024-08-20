bf4ac13440ac576a6023120a5ce6c91621957dc3
Swetha B S <swetha@multicorewareinc.com>
2024-08-20 03:09:49 +0000

    Add last batch info for all the video readers

--- rocAL_pybind/amd/rocal/readers.py
+++ rocAL_pybind/amd/rocal/readers.py
@@ -189,7 +189,7 @@ def caffe2(path, bbox=False, stick_to_shard=False, pad_last_batch=False):
 def video(sequence_length, file_list_frame_num=False, file_root="", image_type=types.RGB, num_shards=1,
           random_shuffle=False, step=1, stride=1, decoder_mode=types.SOFTWARE_DECODE, enable_frame_num=False,
           enable_timestamps=False, file_list="", stick_to_shard=False, pad_last_batch=False,
-          file_list_include_preceding_frame=False, normalized=False, skip_vfr_check=False):
+          file_list_include_preceding_frame=False, normalized=False, skip_vfr_check=False, last_batch_policy=types.LAST_BATCH_FILL, last_batch_padded=True):
     """!Creates a VideoDecoder node for loading video sequences.
 
         @param sequence_length                      Number of frames in video sequence.
@@ -235,7 +235,8 @@ def video(sequence_length, file_list_frame_num=False, file_root="", image_type=t
         "loop": False,
         "frame_step": step,
         "frame_stride": stride,
-        "file_list_frame_num": file_list_frame_num}  # VideoDecoder
+        "file_list_frame_num": file_list_frame_num,
+        "last_batch_info": (last_batch_policy, last_batch_padded)}  # VideoDecoder
     videos = b.videoDecoder(
         Pipeline._current_pipeline._handle, *(kwargs_pybind_decoder.values()))
     return (videos)
@@ -248,7 +249,8 @@ def video_resize(sequence_length, resize_width, resize_height, file_list_frame_n
                  scaling_mode=types.SCALING_MODE_DEFAULT, interpolation_type=types.LINEAR_INTERPOLATION,
                  resize_longer=0, resize_shorter=0, max_size=[], enable_frame_num=False,
                  enable_timestamps=False, file_list="", stick_to_shard=False, pad_last_batch=False,
-                 file_list_include_preceding_frame=False, normalized=False, skip_vfr_check=False):
+                 file_list_include_preceding_frame=False, normalized=False, skip_vfr_check=False, 
+                 last_batch_policy=types.LAST_BATCH_FILL, last_batch_padded=True):
     """!Creates a VideoDecoderResize node in the pipeline for loading and resizing video sequences.
 
         @param sequence_length                      Number of frames in video sequence.
@@ -294,13 +296,13 @@ def video_resize(sequence_length, resize_width, resize_height, file_list_frame_n
                              "sequence_length": sequence_length, "resize_width": resize_width, "resize_height": resize_height,
                              "shuffle": random_shuffle, "is_output": False, "loop": False, "frame_step": step, "frame_stride": stride,
                              "file_list_frame_num": file_list_frame_num, "scaling_mode": scaling_mode, "max_size": max_size,
-                             "resize_shorter": resize_shorter, "resize_longer": resize_longer, "interpolation_type": interpolation_type}
+                             "resize_shorter": resize_shorter, "resize_longer": resize_longer, "interpolation_type": interpolation_type, "last_batch_info": (last_batch_policy, last_batch_padded)}
     videos = b.videoDecoderResize(
         Pipeline._current_pipeline._handle, *(kwargs_pybind_decoder.values()))
     return (videos, meta_data)
 
 
-def sequence_reader(file_root, sequence_length, image_type=types.RGB, num_shards=1, random_shuffle=False, step=3, stride=1, stick_to_shard=False, pad_last_batch=False):
+def sequence_reader(file_root, sequence_length, image_type=types.RGB, num_shards=1, random_shuffle=False, step=3, stride=1, stick_to_shard=False, pad_last_batch=False, last_batch_policy=types.LAST_BATCH_FILL, last_batch_padded=True):
     """!Creates a SequenceReader node for loading image sequences.
 
         @param file_root            Root directory containing image sequences.
@@ -326,7 +328,8 @@ def sequence_reader(file_root, sequence_length, image_type=types.RGB, num_shards
         "shuffle": random_shuffle,
         "loop": False,
         "frame_step": step,
-        "frame_stride": stride}
+        "frame_stride": stride,
+        "last_batch_info": (last_batch_policy, last_batch_padded)}
     frames = b.sequenceReader(
         Pipeline._current_pipeline._handle, *(kwargs_pybind.values()))
     return (frames)
