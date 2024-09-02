fdfd6e511ac478d54ce013897e8149eddd9dee43
Swwetha B S <swetha@multicorewareinc.com>
2024-09-02 11:21:42 +0000

    Minor change for shuffle in webdataset reader

--- rocAL/source/readers/webdataset_source_reader.cpp
+++ rocAL/source/readers/webdataset_source_reader.cpp
@@ -109,7 +109,8 @@ Reader::Status WebDatasetSourceReader::initialize(ReaderConfig desc) {
     _curr_file_idx = get_start_idx(); // shard's start_idx would vary for every shard in the vector
     // shuffle dataset if set
     if (ret == Reader::Status::OK && _shuffle)
-        std::random_shuffle(_file_names.begin(), _file_names.end());
+        std::random_shuffle(_all_shard_file_names_padded.begin() + get_start_idx(), _all_shard_file_names_padded.begin() + get_start_idx() + actual_shard_size_without_padding());
+
     return ret;
 }
 
