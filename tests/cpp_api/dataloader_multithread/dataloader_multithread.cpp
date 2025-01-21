731f7df15d224f67dccc42e81fe211a876382306
fiona-gladwin <fionagladwin@multicorewareinc.com>
2025-01-21 03:18:46 +0000

    Minor change

--- tests/cpp_api/dataloader_multithread/dataloader_multithread.cpp
+++ tests/cpp_api/dataloader_multithread/dataloader_multithread.cpp
@@ -189,7 +189,7 @@ int main(int argc, const char **argv) {
     // check command-line usage
     const int MIN_ARG_COUNT = 2;
     if (argc < MIN_ARG_COUNT) {
-        std::cout << "Usage: dataloader_multithread <image_dataset_folder/video_folder - required> <num_gpus - 1 (gpu)/cpu=0> " <<
+        std::cout << "Usage: dataloader_multithread <image_dataset_folder - required> <num_gpus - 1 (gpu)/cpu=0> " <<
                     "num_shards decode_width decode_height batch_size shuffle display_on_off dec_mode<0(tjpeg)/1(opencv)/2(hwdec)>" << std::endl;
         return -1;
     }
