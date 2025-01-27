e8dcf236b5ed882907a1a5c807fdf76628c7412d
kiritigowda <kiritigowda@gmail.com>
2025-01-27 15:02:00 -0800

    Setup - Install LibTar for all OS

--- rocAL-setup.py
+++ rocAL-setup.py
@@ -392,8 +392,7 @@ else:
 
     # libtar - https://repo.or.cz/libtar.git ; version - v1.2.20
     libtar_version = 'v1.2.20'
-    if "Ubuntu-22" in platformInfo or "Ubuntu-24" in platformInfo:
-        ERROR_CHECK(os.system('sudo '+linuxFlag+' '+linuxSystemInstall+' '+linuxSystemInstall_check +
+    ERROR_CHECK(os.system('sudo '+linuxFlag+' '+linuxSystemInstall+' '+linuxSystemInstall_check +
                         ' install autoconf libtool'))
     ERROR_CHECK(os.system(
         '(cd '+deps_dir+'; git clone -b '+ libtar_version+' https://repo.or.cz/libtar.git )'))
