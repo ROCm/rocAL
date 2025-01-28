da80f42a95eea8eb84c0c2f8344a74b4aa9a20a6
kiritigowda <kiritigowda@gmail.com>
2025-01-27 16:28:00 -0800

    Setup - Audio fix for SLES

--- rocAL-setup.py
+++ rocAL-setup.py
@@ -270,19 +270,21 @@ coreDebianPackages = [
     'protobuf-compiler'
 ]
 
+libsndFile = "libsndfile-devel"
 libPythonProto = "python3-protobuf"
 libProtoCompiler = "protobuf-compiler"
 if "centos" in os_info_data and "VERSION_ID=7" in os_info_data:
     libPythonProto = "protobuf-python"
 if "SLES" in os_info_data:
     libProtoCompiler = "libprotobuf-c-devel"
+    libsndFile = "cmake" # TBD - libsndfile-devel  fails to install in SLES
 coreRPMPackages = [
     'nasm',
     'yasm',
     'lmdb-devel',
     'jsoncpp-devel',
     #'rapidjson-devel',
-    'libsndfile-devel', # for audio features
+    str(libsndFile), # for audio features
     'python3-devel',
     'python3-pip',
     str(libPythonProto),
