4610cf26a7ec95e06595ee4b8fd8ab4101abec55
fiona-gladwin <fionagladwin@multicorewareinc.com>
2025-02-07 19:42:21 +0000

    Minor fix file source reader

--- rocAL/source/readers/file_source_reader.cpp
+++ rocAL/source/readers/file_source_reader.cpp
@@ -55,6 +55,7 @@ Reader::Status FileSourceReader::initialize(ReaderConfig desc) {
     _stick_to_shard = _sharding_info.stick_to_shard;
     _shard_size = _sharding_info.shard_size;
     ret = subfolder_reading();
+    _curr_file_idx = _shard_start_idx_vector[_shard_id]; // shard's start_idx would vary for every shard in the vector
     // shuffle dataset if set
     if (ret == Reader::Status::OK && _shuffle)
         std::random_shuffle(_file_names.begin() + _shard_start_idx_vector[_shard_id],
