661859ffac301ebe176d14eb29be055e69b9fb00
LakshmiKumar23 <lakshmi.kumar@amd.com>
2025-03-13 10:57:24 -0700

    update dlpack package to match develop branch

--- rocAL-setup.py
+++ rocAL-setup.py
@@ -382,7 +382,7 @@ else:
     elif "redhat" in platformInfo:
         # no package avialable -- using source
         ERROR_CHECK(os.system('sudo '+linuxFlag+' '+linuxSystemInstall+' '+linuxSystemInstall_check +
-                        ' install https://rpmfind.net/linux/opensuse/tumbleweed/repo/oss/x86_64/dlpack-devel-0.8-1.5.x86_64.rpm'))
+                        ' install https://rpmfind.net/linux/opensuse/distribution/leap/16.0/repo/oss/x86_64/dlpack-devel-0.8-lp160.1.1.x86_64.rpm'))
 
 
     # RapidJSON - Source TBD: Package install of RapidJSON has compile issues - https://github.com/Tencent/rapidjson.git -- master
