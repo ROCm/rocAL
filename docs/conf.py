04d5aeb5730ac869fc862841ba250375e528055f
kiritigowda <kiritigowda@gmail.com>
2025-04-28 10:19:57 -0700

    Copyright - year updates

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # MIT License
 
-# Copyright (c) 2022 - 2023 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (c) 2022 - 2025 Advanced Micro Devices, Inc. All rights reserved.
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -41,7 +41,7 @@ left_nav_title = f"rocAL {version_number} Documentation"
 # for PDF output on Read the Docs
 project = "rocAL Documentation"
 author = "Advanced Micro Devices, Inc."
-copyright = "Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved."
+copyright = "Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved."
 version = version_number
 release = version_number
 
