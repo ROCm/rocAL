229f82676e3c9e1438ed45aa1f7756a6987364a0
SundarRajan28 <sundarrajan@multicorewareinc.com>
2025-08-07 10:05:22 -0400

    Resolve review comment

--- rocAL/include/parameters/parameter_factory.h
+++ rocAL/include/parameters/parameter_factory.h
@@ -104,7 +104,7 @@ class ParameterFactory {
    private:
     long long unsigned _seed;
     std::set<pParamCore> _parameters;  //<! Keeps the random generators used to randomized the augmentation parameters
-    std::set<pParam> _params;  //<! Keeps the Params used for augmentation parameters
+    std::set<pParam> _params;          //<! Used for storing IntParam and FloatParam objects
     static ParameterFactory* _instance;
     static std::mutex _mutex;
     ParameterFactory();
