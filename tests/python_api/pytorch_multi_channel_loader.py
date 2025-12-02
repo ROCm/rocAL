f65eddb667da8355d5a759c473122afce7e894b6
SundarRajan28 <sundarrajan@multicorewareinc.com>
2025-12-02 02:44:05 -0600

    Resolve review comments

--- tests/python_api/pytorch_multi_channel_loader.py
+++ tests/python_api/pytorch_multi_channel_loader.py
@@ -31,7 +31,7 @@ import timeit
 
 def main():
     if len(sys.argv) < 3:
-        print("Please pass cpu/gpu batch_size")
+        print("Please pass cpu/gpu batch_size input_channels(optional)")
         exit(0)
     try:
         path = "output_folder/"
@@ -42,27 +42,28 @@ def main():
         print(error)
     rocal_cpu = True if sys.argv[1] == "cpu" else False
     batch_size = int(sys.argv[2])
+    input_channels = int(sys.argv[3]) if len(sys.argv) == 4 else 16 # Defaulting to 16 channels if not provided
 
     local_rank = 0
     world_size = 1
 
     data_path = 'dummy_inputs'
+    input_shape = (224, 224, input_channels)
     if os.path.exists(data_path):
         shutil.rmtree(data_path)
     os.makedirs(data_path)
     for i in range(10):
-        # Creating dummy inputs similar to data shape of dataset used in Deepcam training
-        np_array = np.random.randint(0, 256, (768, 1152, 16))
+        # Creating random numpy arrays with specified number of channels
+        np_array = np.random.randint(0, 256, size=input_shape)
         np.save(os.path.join(data_path, f'{i}.npy'), np_array.astype(np.float32))
 
+    # This pipeline reads multi channel numpy files, normalizes the values to (-1,1) and transposes them
     pipe = Pipeline(batch_size=batch_size, num_threads=8, device_id=local_rank, seed=local_rank+10, rocal_cpu=rocal_cpu, tensor_dtype=types.FLOAT,
                     tensor_layout=types.NCHW, prefetch_queue_depth=6, output_memory_type=types.HOST_MEMORY if rocal_cpu else types.DEVICE_MEMORY)
-    # This pipeline is similar to the Deepcam dataset loader
-    # Reference: Deepcam Dataset loader - https://github.com/mlcommons/hpc/blob/main/deepcam/src/deepCam/data/cam_hdf5_dataset.py#L136
     with pipe:
         numpy_reader_output = fn.readers.numpy(file_root=data_path, output_layout=types.NHWC, shard_id=local_rank, num_shards=world_size, random_shuffle=True, seed=local_rank)
-        # Using data mean and stddev as 127.5 since the original deepcam mean and stddev cannot be used with the dummy inputs
-        normalized_output = fn.normalize(numpy_reader_output, axes=[0,1], mean=[127.5]*16, stddev=[127.5]*16, output_datatype=types.FLOAT)
+        # Using data mean and stddev as 127.5 to normalize between -1 and 1
+        normalized_output = fn.normalize(numpy_reader_output, axes=[0,1], mean=[127.5]*input_shape[-1], stddev=[127.5]*input_shape[-1], output_datatype=types.FLOAT)
         transposed_output = fn.transpose(normalized_output, perm=[2,0,1], output_layout=types.NCHW, output_dtype=types.FLOAT)
         pipe.set_outputs(transposed_output)
 
