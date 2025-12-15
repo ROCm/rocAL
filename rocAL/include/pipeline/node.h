263029a241cd6abd5912821a81fe962b5e674be5
fiona-gladwin <fionagladwin@multicorewareinc.com>
2025-12-15 12:52:58 -0600

    Add comments for the get function
    Improve the THROW statements

--- rocAL/include/pipeline/node.h
+++ rocAL/include/pipeline/node.h
@@ -56,8 +56,14 @@ class Node {
     const std::vector<Argument>& get_args_list() const { return _args; }
     // Returns the LoaderModule associated with this node, Derived LoaderNodes should override this method.
     virtual std::shared_ptr<LoaderModule> get_loader_module() { THROW("get_loader_module() is not implemented for the Node"); }
-    virtual void initialize_args(std::vector<Argument> &arguments, std::shared_ptr<MetaDataReader> meta_data_reader) { THROW("Not Implemented"); }
-    virtual void initialize_args(std::vector<Argument> &arguments) { THROW("Not Implemented"); }
+    virtual void initialize_args(std::vector<Argument> &arguments, std::shared_ptr<MetaDataReader> meta_data_reader) { 
+        THROW("initialize_args not implemented for the LoaderNode type: " + node_name() + 
+              ". Derived LoaderNode must override this method to handle deserialization with metadata reader."); 
+    }
+    virtual void initialize_args(std::vector<Argument> &arguments) { 
+        THROW("initialize_args(arguments) not implemented for node type: " + node_name() + 
+              ". Derived Nodes must override this method to handle deserialization.");
+    }
 
    protected:
     virtual void create_node() = 0;
