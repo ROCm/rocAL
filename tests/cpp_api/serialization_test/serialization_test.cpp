8b03b7853153d5c08e4bf74581394f5b67d49834
fiona-gladwin <fionagladwin@multicorewareinc.com>
2025-12-16 04:19:23 -0600

    Fix opencv include in serialize test

--- tests/cpp_api/serialization_test/serialization_test.cpp
+++ tests/cpp_api/serialization_test/serialization_test.cpp
@@ -30,15 +30,17 @@ THE SOFTWARE.
 #include <memory>
 
 #include "rocal_api.h"
+#include "opencv2/opencv.hpp"
+using namespace cv;
 
-#ifdef USE_OPENCV_4
-#include <opencv2/opencv.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-#else
-#include <opencv2/opencv.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
+#if USE_OPENCV_4
+#define CV_LOAD_IMAGE_COLOR IMREAD_COLOR
+#define CV_BGR2GRAY COLOR_BGR2GRAY
+#define CV_GRAY2RGB COLOR_GRAY2RGB
+#define CV_RGB2BGR COLOR_RGB2BGR
+#define CV_FONT_HERSHEY_SIMPLEX FONT_HERSHEY_SIMPLEX
+#define CV_FILLED FILLED
+#define CV_WINDOW_AUTOSIZE WINDOW_AUTOSIZE
 #endif
 
 int main(int argc, const char **argv) {
