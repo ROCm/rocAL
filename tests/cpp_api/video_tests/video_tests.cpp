f0c5857281dfa1187dc6bfb462c500c2c7fdbadf
daniellegillai <dagillai@amd.com>
2026-07-08 10:46:23 -0700

    rocdecode test fix

--- tests/cpp_api/video_tests/video_tests.cpp
+++ tests/cpp_api/video_tests/video_tests.cpp
@@ -130,7 +130,7 @@ int main(int argc, const char **argv) {
     if (argc >= argIdx + MIN_ARG_COUNT)
         enable_sequence_rearrange = atoi(argv[++argIdx]) ? true : false;
 
-    auto decoder_mode = ((decoder_type > 0) ? RocalDecodeDevice::ROCAL_HW_DECODE : RocalDecodeDevice::ROCAL_SW_DECODE);
+    auto decoder_mode = ((decoder_type == 1) ? RocalDecodeDevice::ROCAL_HW_DECODE : RocalDecodeDevice::ROCAL_SW_DECODE);
     RocalDecoderType rocal_decoder_type = RocalDecoderType::ROCAL_DECODER_VIDEO_FFMPEG_SW;
     if (decoder_type == 1) {
         rocal_decoder_type = RocalDecoderType::ROCAL_DECODER_VIDEO_ROCDECODE;
